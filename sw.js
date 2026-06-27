762d4b004f8ae28669900cda2ae75b7e20927882
pcal3278-blip <pcal3278@gmail.com>
2026-06-27 11:59:09 -0400

    Update service worker for v6.0 LIVE

--- sw.js
+++ sw.js
@@ -1,27 +1,24 @@
-const CACHE_NAME = 'cp-command-center-v5-2-1-20260627';
+const CACHE_NAME = 'cp-command-center-v6-0-live-20260627';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()).catch(() => self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).finally(() => self.skipWaiting()));
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
-});
-self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   if (event.request.mode === 'navigate') {
-    event.respondWith(fetch(event.request).then(response => {
-      const copy = response.clone();
+    event.respondWith(fetch(event.request).then(res => {
+      const copy = res.clone();
       caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy)).catch(() => null);
-      return response;
+      return res;
     }).catch(() => caches.match('./index.html')));
     return;
   }
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone();
+  event.respondWith(fetch(event.request).then(res => {
+    const copy = res.clone();
     caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => null);
-    return response;
-  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
+    return res;
+  }).catch(() => caches.match(event.request)));
 });
