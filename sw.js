77038f6eb6d6e0027280e76a941dfd2b6404afa9
pcal3278-blip <pcal3278@gmail.com>
2026-07-15 03:01:54 -0400

    Refresh sw.js for 7.3.1

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = "cp-command-center-7.3.0-neural-reader";
+const CACHE_NAME = "cp-command-center-7.3.1-neural-reader";
 const APP_SHELL = [
   "./",
-  "./styles-v6.css?v=7.3.0",
-  "./app-core-base-v6.js?v=7.3.0",
-  "./app-core-ui-v6.js?v=6.1.0",
-  "./app-data-v6.js?v=7.3.0",
-  "./app-live-v6.js?v=6.1.0",
+  "./styles-v6.css?v=7.3.1",
+  "./app-core-base-v6.js?v=7.3.1",
+  "./app-core-ui-v6.js?v=7.3.1",
+  "./app-data-v6.js?v=7.3.1",
+  "./app-live-v6.js?v=7.3.1",
   "./cast25-built-in.js?v=2026-07-11-1",
   "./readings/cast25-2026-07-11.txt?v=2026-07-11-1",
-  "./manifest-v6.webmanifest?v=7.3.0",
+  "./manifest-v6.webmanifest?v=7.3.1",
   "./icon-v6.svg"
 ];
 
